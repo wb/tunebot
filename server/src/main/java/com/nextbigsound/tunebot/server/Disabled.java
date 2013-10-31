234a2eb48c7330e6bcf55479ce5f7b1cab27fca2
Walter Blaurock <walter@nextbigsound.com>
2013-10-31 12:12:17 -0400

    Setting up directory structure for go.

--- server/src/main/java/com/nextbigsound/tunebot/server/Disabled.java
+++
@@ -1,9 +0,0 @@
-package com.nextbigsound.tunebot.server;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface Disabled {
-
-}
